dfcc7b4c8563c234187e98418d98b4e52e7dc47c
Canyon Smith <canyonfsmith@gmail.com>
2021-06-09 14:15:50 -0600

    added new texting format

--- send_text.py
+++ send_text.py
@@ -7,7 +7,16 @@ account_sid = 'AC4edaa4f9768eb268b7907e9c2680d55d'
 auth_token = '22cb9fa604d5051a11787b431f79201d'
 client = Client(account_sid, auth_token)
 
-client_phone_contact = ['+18016237631','+18018752975']
+client_phone_contact = ['+18016237631','+18018752975','+13852673595']
+def check():
+    with open("bought.txt", "r") as stock_name:
+        bought=[i.strip() for line in stock_name for i in line.split(',') if i]
+        print(bought)
+        return bought
+
+def add_stock(stock):
+    with open("bought.txt", "a") as stock_name:
+        stock_name.write("\n"+stock)
 
 def send_text_of_json(stock):
     five_minute_recommendation = stock_analysis(stock,'1h')
@@ -19,22 +28,62 @@ def send_text_of_json(stock):
                 one_minute_recommendation = stock_analysis(stock,'1m')
                 
                 current_price, previous_close = extract_stock_previous_and_current_price(stock)
-                break_through_report, breakthrough_level = create_resistance_report(stock, current_price=current_price, previous_close=previous_close)
+                current_price = current_price.__round__(2)
+                break_through_report, breakthrough_level, time_interval, resistance, next_resistance = create_resistance_report(stock, current_price=current_price, previous_close=previous_close)
+                # print(next_highest_resistance)
+                print(next_resistance)
+                
                 if breakthrough_level == 'True':
+                    resistance = float(resistance)
+                    current_price = float(current_price)
+                    def calculate_stock_percentage_gain_today(current_price=current_price, previous_close=previous_close):
+                        previous_close = float(previous_close)
+                        day_gain = (((current_price/previous_close) - 1) * 100).__round__(2)
+                        return day_gain
+                    
+                    def calculate_stock_percentage_drop_needed_for_breaking_resistance(current_price=current_price,resistance=resistance):
+                        resistance = float(resistance)
+                        drop_needed = ((-(resistance/current_price)+1) * 100).__round__(2)
+                        print("drop needed",drop_needed)
+                        return drop_needed
 
-                    def send_client_text(phone_number):
-                        message = client.messages.create(
-                                                body="Stock: " + stock +
-                                                    "\n Break through: " + break_through_report +
-                                                    "\n One Minute: " + one_minute_recommendation +
-                                                    "\n Five Minute: " + five_minute_recommendation +
-                                                    "\n Fifteen Minute: " + fifteen_minute_recommendation +
-                                                    "\n One Hour: " + one_hour_recommendation,
-                                                from_='+13852336341',
-                                                to=phone_number
-                                            )
-                    for phone_number in client_phone_contact:
-                        send_client_text(phone_number)
+                    def calculate_stock_gain_needed_to_break_next_resistance(current_price=current_price,next_resistance=next_resistance):
+                        next_resistance = float(next_resistance).__round__(2)
+                        print(next_resistance)
+                        gain_needed = ((-(current_price/next_resistance) + 1) * 100).__round__(2)
+                        print("gain needed",gain_needed)
+                        return gain_needed
+                       
+                    day_gain = calculate_stock_percentage_gain_today(current_price=current_price,previous_close=previous_close)
+                    drop_needed = calculate_stock_percentage_drop_needed_for_breaking_resistance()
+                    gain_needed = calculate_stock_gain_needed_to_break_next_resistance()
+                    
+                    bought_list = check()
+                    if stock not in bought_list:
+                    
+
+                        def send_client_text(phone_number):
+                            message = client.messages.create(
+                                                    body=" Trade Alert: " + stock + " $" + str(current_price) + "         +" + str(day_gain) + "%" +
+                                                    # body=" Trade Alert: " + "                                 " + stock +
+                                                        "\n============================" +
+                                                        # "\n           Price: $"  + str(current_price) + " " + "             (+" + str(day_gain) + "%)" +
+                                                        # "\n" + "(" + time_interval + ")" + " Resistance: $" + str(next_resistance) + " (+" + str(gain_needed) + "%)." +
+                                                        "\n" + " Resistance: $" + str(next_resistance) + "             (+" + str(gain_needed) + "%)" +
+                                                        "\n" + "(" + time_interval + ")" + "Support: $" + str(resistance) + "             (-" + str(drop_needed) + "%)",
+                                                        # "\n One Minute: " + one_minute_recommendation +
+                                                        # "\n Five Minute: " + five_minute_recommendation +
+                                                        # "\n Fifteen Minute: " + fifteen_minute_recommendation +
+                                                        # "\n One Hour: " + one_hour_recommendation,
+                                                    from_='+13852336341',
+                                                    to=phone_number
+                                                )
+                        for phone_number in client_phone_contact:
+                            send_client_text(phone_number)
+                        add_stock(stock)
+                    else:
+                        print("Already sent text for ",stock)
+
             else:
                 pass
         else:
--- test_next_resistance.py
+++ test_next_resistance.py
@@ -19,13 +19,16 @@ def get_resistance_levels(ticker, interval):
     resistance_levels = resistance_levels['levels']
     print(resistance_levels)
     return resistance_levels
-
+i = 0
 def check_if_resistance_broken(ticker, interval, current_price, previous_close):
-    try:
-        resistance_levels = get_resistance_levels(ticker,interval)
+    resistance_levels = get_resistance_levels(ticker,interval)
+    print(len(resistance_levels))
+    if len(resistance_levels) == 0:
+        json = {"break_through": "False","price":current_price,"Resistance": "No Resistance Exists","time_interval":interval}
+        print(json)
+    else:
         i = 0
         for level in resistance_levels:
-            level = float(level).__round__(2)
             i += 1
             if current_price < level:
                 json = {"break_through": "False","price":current_price,"next_highest_resistance":level,"time_interval":interval}
@@ -38,16 +41,11 @@ def check_if_resistance_broken(ticker, interval, current_price, previous_close):
                     continue
                 else:
                     next_highest_resistance = resistance_levels[i]
-                    next_highest_resistance = float(next_highest_resistance).__round__(2)
                     json = {"break_through": "True","price":current_price,"Resistance":level,"time_interval":interval,"next_highest_resistance": next_highest_resistance}
                     print(next_highest_resistance)
                     print(json)
                     break
-                
-        return json
-    except:
-        Exception
-        json = {"break_through": "No Resistance Found for this level"}
+        
         return json
 
 def get_all_resistance_levels(ticker,current_price,previous_close):
@@ -57,8 +55,13 @@ def get_all_resistance_levels(ticker,current_price,previous_close):
         resistance_level_bank.append(check_if_resistance_broken(ticker,level,current_price,previous_close))
 
     for resistance_level in resistance_level_bank:
-        if resistance_level['break_through'] == 'True':
-            return resistance_level
+        try:
+            print(resistance_level['break_through'])
+        except:
+            Exception
+            print(Exception)
+        # if resistance_level['break_through'] == 'True':
+        #     return resistance_level
 
 def create_resistance_report(ticker,current_price,previous_close):
     get_resistance = get_all_resistance_levels(ticker,current_price,previous_close)
@@ -67,9 +70,8 @@ def create_resistance_report(ticker,current_price,previous_close):
         break_through = get_resistance['break_through']
         time_interval = get_resistance['time_interval']
         price = get_resistance['price']
-        next_resistance = get_resistance['next_highest_resistance']
         words = " crossed resistance today at " + str(resistance) + " for " + time_interval + " time interval. Price is " + str(price) + "."
-        return words, break_through, time_interval, resistance, next_resistance
+        return words, break_through, time_interval, resistance
 
     except:
         Exception
@@ -77,8 +79,7 @@ def create_resistance_report(ticker,current_price,previous_close):
         words = "No resistance broken"
         time_interval = " "
         resistance = " "
-        next_resistance = " "
-        return words, break_through, time_interval, resistance, next_resistance
+        return words, break_through, time_interval, resistance
 def calculate_stock_percentage_gain_today():
     cp, pc = extract_stock_previous_and_current_price('APPN')
     cp = float(cp)
@@ -87,3 +88,8 @@ def calculate_stock_percentage_gain_today():
     print(day_gain,"%")
 
 # calculate_stock_percentage_gain_today()
+def yeet(ticker):
+    
+    current_price, previous_close = extract_stock_previous_and_current_price(ticker)
+    create_resistance_report(ticker,current_price,previous_close)
+yeet('SDC')
