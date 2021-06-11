96c819be299b9853345e351a4f06bddb058ebc5b
shefman <shefman8@gmail.com>
2021-06-11 17:26:38 +0100

    add scheduler

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
-from send_text import analyze_equities
-from ScrapedStocks.webscrape_most_shorted_stocks import scrape_most_shorted_stocks
+import time
+from datetime import datetime
+
+import schedule
+
 from ScrapedStocks.scrape_iv_stocks import scrape_highest_iv_stocks
+from ScrapedStocks.webscrape_most_shorted_stocks import \
+    scrape_most_shorted_stocks
+from send_text import analyze_equities
 
 most_shorted_stocks = scrape_most_shorted_stocks()
 iv_stocks = scrape_highest_iv_stocks()
 
 with open("ScrapedStocks/150_motley_fool_stocks.txt", "r") as stock_name:
-    stock_list=[i.strip() for line in stock_name for i in line.split(',') if i]  
+    stock_list = [i.strip()
+                  for line in stock_name for i in line.split(',') if i]
+
 
 def main():
     while True:
         for stock in iv_stocks:
-            print("\n" + "\n",stock)
+            print("\n" + "\n", stock)
             analyze_equities(stock)
         for stock in stock_list:
-            print("\n" + "\n",stock)
+            print("\n" + "\n", stock)
             analyze_equities(stock)
         for stock in most_shorted_stocks:
-            print("\n" + "\n",stock)
+            print("\n" + "\n", stock)
             analyze_equities(stock)
 
+
+task = schedule.every().day.at("13:30").do(main)
+
 if __name__ == '__main__':
-    main()
+    while True:
+        schedule.run_pending()
+        now = datetime.now()
+        if now.hour == 20 and now.minute == 1:
+            schedule.cancel_job(task)
+            print("Sleeping...")
+        time.sleep(1)
+        # main()
