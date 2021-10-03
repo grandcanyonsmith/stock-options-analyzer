890b4925c1d1f63cd16fd06a5c107ceb1c5a2e76
Canyon Smith <canyonfsmith@gmail.com>
2021-10-03 09:56:17 -0600

    Update webscrape_most_shorted_stocks.py

--- ScrapedStocks/webscrape_most_shorted_stocks.py
+++ ScrapedStocks/webscrape_most_shorted_stocks.py
@@ -10,7 +10,6 @@ from operator import itemgetter
 def scrape_most_shorted_stocks():
     page = requests.get('https://www.marketwatch.com/tools/screener/short-interest')
     soup = BeautifulSoup(page.content, 'html.parser')
-    bs = soup.prettify()
     scraped_most_shorted = soup.find_all('div', class_="cell__content fixed--cell")
     most_shorted_stocks = [stock.text.strip() for stock in scraped_most_shorted]
     most_shorted_stocks.pop(0)
